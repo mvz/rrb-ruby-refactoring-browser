6032a401cb8e6d32691c0c211f97d4a0a8a17cb8
ohai <>
2003-03-02 14:27:21 +0000

    Enumerable#injectを使えるようにした

--- lib/rrb/utils.rb
+++ lib/rrb/utils.rb
@@ -8,9 +8,34 @@ module Enumerable
       ary.sort! { |a, b| a[0] <=> b[0] }
       ary.map! { |i| i[1] }
     end
-    
+
+    def inject(*argv)
+      argc = argv.size
+      
+      if argc == 0
+	first = true
+	result = nil
+
+	each { |e|
+	  if first
+	    first = false
+	    result = e
+	  else
+	    result = yield(result, e)
+	  end
+	}
+      elsif argc == 1
+	result = argv[0]
+	
+	each { |e| result = yield(result, e) }
+      else
+	raise ArgumentError, "wrong # of arguments(#{argc} for 1)"
+      end
+      
+      result
+    end
   end
-  
+    
 end
 
 class IO
