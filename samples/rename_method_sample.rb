9196d772eca4c1a88ec74ab677c39f63f84d04cd
ohai <>
2003-04-06 09:19:03 +0000

    rename method all のテスト用のファイル

---
+++ samples/rename_method_sample.rb
@@ -0,0 +1,47 @@
+
+class A
+
+  def initialize
+    @i = 0
+  end
+  
+  def foo( x )
+    @i += x
+  end
+
+  def baz
+    @i
+  end
+  
+end
+
+class B < A
+
+  def foo( x )
+    @i += x*2
+  end
+
+  def bar
+    hek = 5
+    foo 1 
+    baz
+  end
+
+  def B.foo
+    obj = Object.new
+    
+    def obj.foo
+      return "foo"
+    end
+
+    obj.foo
+  end
+  
+end
+
+if __FILE__ == $0 then
+  obj = B.new
+  
+  obj.foo
+  p obj.bar
+end
---
+++ samples/rename_method_sample_after.rb
@@ -0,0 +1,47 @@
+
+class A
+
+  def initialize
+    @i = 0
+  end
+  
+  def feefoo( x )
+    @i += x
+  end
+
+  def baz
+    @i
+  end
+  
+end
+
+class B < A
+
+  def feefoo( x )
+    @i += x*2
+  end
+
+  def bar
+    hek = 5
+    feefoo 1 
+    baz
+  end
+
+  def B.feefoo
+    obj = Object.new
+    
+    def obj.feefoo
+      return "foo"
+    end
+
+    obj.feefoo
+  end
+  
+end
+
+if __FILE__ == $0 then
+  obj = B.new
+  
+  obj.feefoo
+  p obj.bar
+end
