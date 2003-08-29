62d64711e98f79b65e99989bfb52dcdebaede776
oxy <>
2003-08-29 10:45:01 +0000

    require 'rrb/rename_class_var'を追加

--- lib/rrb/emacs_interface.rb
+++ lib/rrb/emacs_interface.rb
@@ -1,6 +1,7 @@
 require 'rrb/script'
 require 'rrb/rename_local_var'
 require 'rrb/rename_instance_var'
+require 'rrb/rename_class_var'
 require 'rrb/rename_global_var'
 require 'rrb/rename_method_all'
 
