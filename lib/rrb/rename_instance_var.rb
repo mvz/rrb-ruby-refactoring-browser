facc1076f6083c5e06cde7a96cdde27ed2b33639
oxy <>
2003-08-29 07:55:09 +0000

    Rename Instance Variableを実行するクラス、関数群 RenameInstanceVarVisitor   新設   check_namespace     ネームスペース、継承関係をしらべて置きかえるべき変数か調べる   rename_instance_var     実際に変数名を置きかえる部分をくくりだしたもの RenameInstanceVarCheckVisitor   新設

---
+++ lib/rrb/rename_instance_var.rb
@@ -0,0 +1,161 @@
+require 'rrb/script'
+
+module RRB
+
+  class RenameInstanceVarVisitor < Visitor
+
+    def initialize( namespace, dumped_info, old_var, new_var )
+      @namespace = namespace
+      @str_namespace = namespace.join('::')
+      @old_var = old_var
+      @new_var = new_var
+      @dumped_info = dumped_info
+      @result = []
+    end
+
+    attr_reader :result
+    
+    def check_namespace(str_namespace)
+      info = @dumped_info[str_namespace]
+      unless info.ancestor_names.include?(@str_namespace) || str_namespace == @str_namespace
+        return false
+      end      
+      return true
+    end
+
+    def rename_instance_var(str_namespace, node)
+      if check_namespace(str_namespace)
+        node.instance_vars.each do |id|
+          if id.name == @old_var then
+            @result <<
+                     Replacer.new( id.lineno, id.pointer, @old_var, @new_var )
+          end
+        end  
+      end
+    end
+
+    def visit_method( namespace, node )
+      str_namespace = namespace.map{|i| i.name}.join('::')
+      rename_instance_var(str_namespace, node)
+    end
+
+    def visit_singleton_method(namespace, node)
+      str_namespace = namespace.map{|i| i.name}.join('::')
+      rename_instance_var(str_namespace, node)
+    end
+
+    def visit_class( namespace, node )
+      str_namespace = namespace.map{|i| i.name}.join('::')
+      if str_namespace.empty?
+        str_namespace = node.name
+      else
+        str_namespace = str_namespace + '::' + node.name
+      end
+      rename_instance_var(str_namespace, node)
+    end
+  end
+
+
+  class RenameInstanceVarCheckVisitor < Visitor
+    
+    def initialize( namespace, dumped_info, old_var, new_var )
+      @namespace = namespace
+      @str_namespace = namespace.join('::')
+      @dumped_info = dumped_info
+      @old_var = old_var
+      @new_var = new_var
+      @result = true
+    end
+
+    attr_reader :result
+
+    def check_namespace(str_namespace)
+      info = @dumped_info[str_namespace]
+      unless info.ancestor_names.include?(@str_namespace) || str_namespace == @str_namespace
+        return false
+      end      
+      return true
+    end
+
+    def rename_instance_var?(str_namespace, node)
+      if check_namespace(str_namespace)
+        node.instance_vars.each do |id|
+          if id.name == @new_var then
+            return false
+          end
+        end  
+      end
+      return true
+    end
+
+    def visit_method( namespace, node )
+      str_namespace = namespace.map{|i| i.name}.join('::')
+      if !rename_instance_var?(str_namespace, node)
+        @result = false
+      end
+    end
+
+    def visit_singleton_method(namespace, node)
+      str_namespace = namespace.map{|i| i.name}.join('::')
+      if !rename_instance_var?(str_namespace, node)
+        @result = false
+      end
+    end
+
+    def visit_class( namespace, node )
+      str_namespace = namespace.map{|i| i.name}.join('::')
+      if str_namespace == ''
+        str_namespace = node.name
+      else
+        str_namespace = str_namespace + '::' + node.name
+      end
+      if !rename_instance_var?(str_namespace, node)
+        @result = false
+      end
+    end
+    
+  end
+
+  class ScriptFile
+    
+    def rename_instance_var( namespace, dumped_info, old_var, new_var )
+      visitor = RenameInstanceVarVisitor.new( namespace, dumped_info,
+					  old_var, new_var )
+      @tree.accept( visitor )
+      @new_script = RRB.replace_str( @input, visitor.result )
+    end
+
+    def rename_instance_var?( namespace, dumped_info, old_var, new_var )
+      return false unless RRB.valid_instance_var?( new_var )
+      visitor = RenameInstanceVarCheckVisitor.new( namespace, dumped_info,
+					       old_var, new_var )
+      @tree.accept( visitor )
+      return visitor.result
+    end
+
+  end
+
+  class Script
+    
+    def rename_instance_var( namespace, old_var, new_var )
+      @files.each do |scriptfile|
+	scriptfile.rename_instance_var( namespace, get_dumped_info,
+				    old_var, new_var )
+      end
+    end
+
+    def rename_instance_var?( namespace, old_var, new_var )
+      @files.each do |scriptfile|
+	if not scriptfile.rename_instance_var?( namespace,get_dumped_info,
+					    old_var, new_var ) then
+	  return false
+	end
+      end
+
+      return true
+      
+    end
+
+  end
+  
+end
