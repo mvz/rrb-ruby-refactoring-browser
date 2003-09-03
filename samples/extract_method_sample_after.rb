bba4f0f4a4828eb1318646715bd83b5b727511a8
oxy <>
2003-09-03 01:42:11 +0000

    Extract Method?で行番号からnamespaceを正しく取得できているかのテストを 追加

--- samples/extract_method_sample_after.rb
+++ samples/extract_method_sample_after.rb
@@ -1,18 +1,20 @@
 
 class A
-  def bar(x, y)
-    z = 2 * x
-    w = 2 * y
-    puts "#{x},#{y}"
-    puts "#{z},#{w}"
+  class B
+    def bar(x, y)
+      z = 2 * x
+      w = 2 * y
+      puts "#{x},#{y}"
+      puts "#{z},#{w}"
+    end
+    def foo
+      x = 1
+      y = 2
+      bar(x, y)
+    end
   end
-  def foo
-    x = 1
-    y = 2
-    bar(x, y)
-  end
-  def foo2
-    x = 1
-    y = 2
+  def bar
   end
 end
+x = 1
+y = 2
