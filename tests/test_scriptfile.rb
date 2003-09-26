2f126e372e9a5743fe21766bd97b1b4ea50b19d5
oxy <>
2003-09-26 12:50:48 +0000

    Move Method, Move Method?のテストを追加

--- tests/test_scriptfile.rb
+++ tests/test_scriptfile.rb
@@ -6,6 +6,7 @@ require 'rrb/rename_local_var'
 require 'rrb/rename_global_var'
 require 'rrb/rename_method_all'
 require 'rrb/extract_method'
+require 'rrb/move_method'
 
 class TestScriptFile < RUNIT::TestCase
 
@@ -116,6 +117,24 @@ class TestScriptFile < RUNIT::TestCase
       assert_equals(false, script_file.extract_method?( 'bar', 4, 5))
       assert_equals(true, script_file.extract_method?( 'bar', 16, 17))
     end    
+  end
+
+  def move_method
+    File.open( 'samples/move_method_sample.rb', 'r' ) do |file|
+      script_file = RRB::ScriptFile.new( file, file.path )
+      script_file.move_method('foo', 'X::B', 'X::A')
+      assert_equals( File.open( 'samples/move_method_sample_after.rb' ).read,		    script_file.new_script )
+    end
+  end
+  def test_move_method?
+    File.open( 'samples/move_method_sample.rb', 'r' ) do |file|
+      script_file = RRB::ScriptFile.new( file, file.path )
+      assert_equals(true, script_file.move_method?('foo', RRB::NS.new('X::B'), RRB::NS.new('X::A')))
+      assert_equals(false, script_file.move_method?('bar', RRB::NS.new('X::B'), RRB::NS.new('X::A')))
+      assert_equals(false, script_file.move_method?('foos', RRB::NS.new('X::B'), RRB::NS.new('X::A')))
+      assert_equals(false, script_file.move_method?('bar', RRB::NS.new('X::A'), RRB::NS.new('X::B')))
+    end    
+
   end
 end
 
