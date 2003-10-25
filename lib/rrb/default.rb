b91e3d338ff9991b139b3f8506d703479b1fcd85
ohai <>
2003-10-25 08:29:50 +0000

    RRB::CONF["RUBY_OPTS"]を使えるようにした。 内部でrubyを呼びだすときのオプションを設定できる。

--- lib/rrb/default.rb
+++ lib/rrb/default.rb
@@ -4,7 +4,8 @@ module RRB
   CONF = {
     'WORK_DIR_BASENAME' => "/tmp/rrb_work",
     'TAB_WIDTH' => 8,
-    'INDENT_LEVEL' => 2
+    'INDENT_LEVEL' => 2,
+    'RUBY_OPTS' => "",
   }
   
   begin
@@ -15,5 +16,5 @@ module RRB
   WORK_DIR_BASENAME = CONF["WORK_DIR_BASENAME"]
   TAB_WIDTH = CONF["TAB_WIDTH"]
   INDENT_LEVEL = CONF["INDENT_LEVEL"]
-    
+  RUBY_OPTS = CONF["RUBY_OPTS"]
 end
