9af802beecf1cf30704354d4799d4cd6db0ac339
oxy <>
2003-10-31 16:01:02 +0000

    継承関係をテストできていなかったのを修正

--- samples/extract_method_sample.rb
+++ samples/extract_method_sample.rb
@@ -1,22 +1,22 @@
 
-class A
-  class C
-    def hogehoge
-    end
-  end
-  class B
-    def foo
-      x = 1
-      y = 2
-      z = 2 * x
-      z = 3 * z
-      w = 2 * y
-      puts "#{x},#{y}"
-      puts "#{z},#{w}"
-    end
-  end
-  def bar
+
+class C
+  def hogehoge
   end
 end
+class B < C
+  def foo
+    x = 1
+    y = 2
+    z = 2 * x
+    z = 3 * z
+    w = 2 * y
+    puts "#{x},#{y}"
+    puts "#{z},#{w}"
+  end
+end
+def bar
+end
+
 x = 1
 y = 2
--- samples/extract_method_sample_after.rb
+++ samples/extract_method_sample_after.rb
@@ -1,26 +1,26 @@
 
-class A
-  class C
-    def hogehoge
-    end
-  end
-  class B
-    def bar(x, y)
-      z = 2 * x
-      z = 3 * z
-      w = 2 * y
-      puts "#{x},#{y}"
-      return z, w
-    end
-    def foo
-      x = 1
-      y = 2
-      z, w = bar(x, y)
-      puts "#{z},#{w}"
-    end
-  end
-  def bar
+
+class C
+  def hogehoge
   end
 end
+class B < C
+  def bar(x, y)
+    z = 2 * x
+    z = 3 * z
+    w = 2 * y
+    puts "#{x},#{y}"
+    return z, w
+  end
+  def foo
+    x = 1
+    y = 2
+    z, w = bar(x, y)
+    puts "#{z},#{w}"
+  end
+end
+def bar
+end
+
 x = 1
 y = 2
