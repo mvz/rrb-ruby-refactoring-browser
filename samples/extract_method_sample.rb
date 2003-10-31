ebc188d3967d3a15b468bf07dbf5f0af8afc07a5
oxy <>
2003-10-31 12:54:41 +0000

    親子関係のチェックを追加

--- samples/extract_method_sample.rb
+++ samples/extract_method_sample.rb
@@ -1,10 +1,15 @@
 
 class A
+  class C
+    def hogehoge
+    end
+  end
   class B
     def foo
       x = 1
       y = 2
       z = 2 * x
+      z = 3 * z
       w = 2 * y
       puts "#{x},#{y}"
       puts "#{z},#{w}"
--- samples/extract_method_sample_after.rb
+++ samples/extract_method_sample_after.rb
@@ -1,16 +1,22 @@
 
 class A
+  class C
+    def hogehoge
+    end
+  end
   class B
     def bar(x, y)
       z = 2 * x
+      z = 3 * z
       w = 2 * y
       puts "#{x},#{y}"
-      puts "#{z},#{w}"
+      return z, w
     end
     def foo
       x = 1
       y = 2
-      bar(x, y)
+      z, w = bar(x, y)
+      puts "#{z},#{w}"
     end
   end
   def bar
