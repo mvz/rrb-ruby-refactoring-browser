31762756cacfb3f145ec2a1edcf419ae70e2df01
ohai <>
2003-11-08 16:31:05 +0000

    RRB::EmacsInterface#parse_argv   ARGVのパースする部分を一部そとに切りだす

--- lib/rrb/emacs_interface.rb
+++ lib/rrb/emacs_interface.rb
@@ -38,69 +38,101 @@ Usage: rrb refactoring-type refactoring-parameter io-type
       return Namespace.new(a), b
     end
 
+    def parse_argv_rename_local_variable(argv)
+      namespace, method_name = split_method_name argv.shift
+      old_var = argv.shift
+      new_var = argv.shift
+      @args = [ namespace, method_name, old_var, new_var ]
+      @refactoring_method = :rename_local_var
+      @check_method = :rename_local_var?
+    end
+    
+    def parse_argv_rename_instance_variable(argv)
+      namespace = Namespace.new( argv.shift )
+      old_var = argv.shift
+      new_var = argv.shift
+      @args = [ namespace, old_var, new_var ]
+      @refactoring_method = :rename_instance_var
+      @check_method = :rename_instance_var?
+    end
+    
+    def parse_argv_rename_class_variable(argv)
+      namespace = Namespace.new( argv.shift )
+      old_var = argv.shift
+      new_var = argv.shift
+      @args = [ namespace, old_var, new_var ]
+      @refactoring_method = :rename_class_var
+      @check_method = :rename_class_var?
+    end
+    
+    def parse_argv_rename_global_variable(argv)
+      old_var = argv.shift
+      new_var = argv.shift
+      @args = [ old_var, new_var ]
+      @refactoring_method = :rename_global_var
+      @check_method = :rename_global_var?
+    end
+    
+    def parse_argv_extract_method(argv)
+      filepath = argv.shift
+      new_method = argv.shift
+      start_lineno = argv.shift.to_i
+      end_lineno = argv.shift.to_i
+      @args = [ filepath, new_method, start_lineno, end_lineno ]
+      @refactoring_method = :extract_method
+      @check_method = :extract_method?
+    end
+    
+    def parse_argv_rename_method_all(argv)
+      old_method = argv.shift
+      new_method = argv.shift
+      @args = [ old_method, new_method ]
+      @refactoring_method = :rename_method_all
+      @check_method = :rename_method_all?
+    end
+    
+    def parse_argv_move_method(argv)
+      method_name = argv.shift
+      old_namespace = Namespace.new( argv.shift )
+      new_namespace = Namespace.new( argv.shift )
+      @args = [ method_name, old_namespace, new_namespace ]
+      @refactoring_method = :move_method
+      @check_method = :move_method?	
+    end
+    
+    def parse_argv_rename_method(argv)
+      old_method = argv.shift
+      new_method = argv.shift
+      for i in 0...(argv.size)
+	break if /^--/ =~ argv[i]
+      end
+      classes = argv[0,i]
+      i.times{ argv.shift }
+      @args = [ classes, old_method, new_method ]
+      @refactoring_method = :rename_method
+      @check_method = :rename_method?
+    end
+    
     def parse_argv( argv )
 
       # analyze REFACTORING-TYPE
       case argv.shift
       when '--rename-local-variable'
-	namespace, method_name = split_method_name argv.shift
-	old_var = argv.shift
-	new_var = argv.shift
-	@args = [ namespace, method_name, old_var, new_var ]
-	@refactoring_method = :rename_local_var
-	@check_method = :rename_local_var?
+	parse_argv_rename_local_variable(argv)
       when '--rename-instance-variable'
-	namespace = Namespace.new( argv.shift )
-	old_var = argv.shift
-	new_var = argv.shift
-	@args = [ namespace, old_var, new_var ]
-	@refactoring_method = :rename_instance_var
-	@check_method = :rename_instance_var?
+	parse_argv_rename_instance_variable(argv)
       when '--rename-class-variablnae'
-	namespace = Namespace.new( argv.shift )
-	old_var = argv.shift
-	new_var = argv.shift
-	@args = [ namespace, old_var, new_var ]
-	@refactoring_method = :rename_class_var
-	@check_method = :rename_class_var?
+        parse_argv_rename_class_variable(argv)
       when '--rename-global-variable'
-	old_var = argv.shift
-	new_var = argv.shift
-	@args = [ old_var, new_var ]
-	@refactoring_method = :rename_global_var
-	@check_method = :rename_global_var?
+        parse_argv_rename_global_variable(argv)
       when '--rename-method-all'
-	old_method = argv.shift
-	new_method = argv.shift
-	@args = [ old_method, new_method ]
-	@refactoring_method = :rename_method_all
-	@check_method = :rename_method_all?
+        parse_argv_rename_method_all(argv)
       when '--extract-method'
-	filepath = argv.shift
-	new_method = argv.shift
-	start_lineno = argv.shift.to_i
-	end_lineno = argv.shift.to_i
-	@args = [ filepath, new_method, start_lineno, end_lineno ]
-	@refactoring_method = :extract_method
-	@check_method = :extract_method?
+        parse_argv_extract_method(argv)
       when '--move-method'
-	method_name = argv.shift
-	old_namespace = Namespace.new( argv.shift )
-	new_namespace = Namespace.new( argv.shift )
-	@args = [ method_name, old_namespace, new_namespace ]
-	@refactoring_method = :move_method
-	@check_method = :move_method?	
+        parse_argv_move_method(argv)
       when '--rename-method'
-	old_method = argv.shift
-	new_method = argv.shift
-	for i in 0...(argv.size)
-	  break if /^--/ =~ argv[i]
-	end
-	classes = argv[0,i]
-	i.times{ argv.shift }
-	@args = [ classes, old_method, new_method ]
-	@refactoring_method = :rename_method
-	@check_method = :rename_method?
+        parse_argv_rename_method(argv)
       else
 	raise RRBError, "Unknown refactoring"
       end
