7e0dc33832e62ff0adb081e566d907f96ae6397f
oxy <>
2004-01-11 11:19:27 +0000

    Push Down Methodの操作を追加

--- lib/rrb/emacs_interface.rb
+++ lib/rrb/emacs_interface.rb
@@ -8,6 +8,7 @@ require 'rrb/rename_constant'
 require 'rrb/extract_method'
 require 'rrb/move_method'
 require 'rrb/pullup_method'
+require 'rrb/pushdown_method'
 
 module RRB
 
@@ -132,6 +133,15 @@ Usage: rrb refactoring-type refactoring-parameter io-type
       @refactoring_method = :pullup_method
       @check_method = :pullup_method?
     end
+
+    def parse_argv_pushdown_method(argv)
+      method_name = argv.shift
+      old_namespace = Namespace.new(argv.shift)
+      new_namespace = Namespace.new(argv.shift)
+      @args = [method_name, old_namespace, new_namespace]
+      @refactoring_method = :pushdown_method
+      @check_method = :pushdown_method?
+    end
     
     def parse_argv( argv )
 
@@ -157,6 +167,8 @@ Usage: rrb refactoring-type refactoring-parameter io-type
         parse_argv_rename_constant(argv)
       when '--pullup-method'
         parse_argv_pullup_method(argv)
+      when '--pushdown-method'
+        parse_argv_pushdown_method(argv)
       else
 	raise RRBError, "Unknown refactoring"
       end
