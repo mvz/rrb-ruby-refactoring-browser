2ccb1dcc8a81f354bf88e504d535edf8d7084d90
oxy <>
2004-01-11 11:21:10 +0000

    Push Down Methodのサンプル(リファクタリング後)

---
+++ samples/pushdown_method_sample_after.rb
@@ -0,0 +1,26 @@
+samples/pushdown_method_sample.rbclass A
+  def a
+  end
+end
+
+class B < A
+  def y
+    z
+  end
+  def z
+  end
+  def w
+  end
+
+  private :z
+end
+
+class C < B
+  def x
+    p 1
+    a
+  end
+  def w
+  end
+end
+-- END --
