c3fa9bd51f3bc9f819771ec31ebc4f56eb14df63
oxy <>
2004-01-23 13:08:45 +0000

    GetClassOnRegionVisitor GetMethodOnRegionVisitor 	指定された行に存在するクラス、関数を調べるVisitor

--- lib/rrb/common_visitor.rb
+++ lib/rrb/common_visitor.rb
@@ -1,6 +1,7 @@
 require 'rrb/node'
 
 module RRB
+
   class GetTargetMethodVisitor < Visitor
     def initialize(namespace, method_name)
       @method_name = method_name
@@ -14,6 +15,51 @@ module RRB
       if namespace.match?(@namespace)
         if node.name == @method_name
           @result_range = node.range
+        end
+      end
+    end
+  end
+
+  class GetClassOnRegionVisitor < Visitor
+    def initialize(start_lineno, end_lineno)
+      @start_lineno = start_lineno
+      @end_lineno = end_lineno
+      @namespace = nil
+    end
+    attr_reader :namespace
+
+    def visit_toplevel(namespace, node)
+      @namespace = namespace
+    end
+
+    def visit_class(namespace, node)
+      if node.range.contain?( @start_lineno .. @end_lineno ) then
+        @namespace = NodeNamespace.new(node, namespace)
+      else
+        unless node.range.out_of?(@start_lineno .. @end_lineno) then
+          @namespace = nil
+        end
+      end
+    end
+  end
+  class GetMethodOnRegionVisitor < Visitor
+    def initialize(start_lineno, end_lineno)
+      @start_lineno = start_lineno
+      @end_lineno = end_lineno
+      @namespace = nil
+    end
+    attr_reader :namespace
+
+    def visit_toplevel(namespace, node)
+      @namespace = namespace
+    end
+
+    def visit_method(namespace, node)
+      if node.range.contain?( @start_lineno .. @end_lineno ) then
+        @namespace = NodeNamespace.new(node, namespace)
+      else
+        unless node.range.out_of?(@start_lineno .. @end_lineno) then
+          @namespace = nil
         end
       end
     end
