08c4510bbf6ce7dc49a8d1be128a9435039ba315
oxy <>
2004-02-01 02:59:00 +0000

    各種リファクタリングのエラーメッセージのユニットテストを追加

--- tests/test_script.rb
+++ tests/test_script.rb
@@ -45,25 +45,6 @@ end
 \C-a-- END --\C-a
 "
 
-  def test_rename_local_var?
-    script = RRB::Script.new_from_io( StringIO.new( RENAME_LOCAL_VAR_INPUT ) )
-    assert_equals( true,
-		  script.rename_local_var?(RRB::NS.new('Rename'),'method_2',
-					   'x','yy') )
-    assert_equals( false,
-		  script.rename_local_var?(RRB::NS.new('Rename'),'method_1',
-					   'x','y') )
-  end
-
-  
-  def test_rename_local_var
-    script = RRB::Script.new_from_io( StringIO.new( RENAME_LOCAL_VAR_INPUT ) )
-    script.rename_local_var( RRB::NS.new('Rename'),'method_2','x','yy' )
-    dst = ''      
-    script.result_to_io( dst )
-    assert_equals( RENAME_LOCAL_VAR_OUTPUT, dst )
-  end
-
   def test_rename_instance_var
     script = RRB::Script.new_from_filenames('samples/rename_instance_var_sample.rb','samples/rename_instance_var_sample2.rb')
     script.rename_instance_var(RRB::NS.new('X::A'), '@a', '@f')
@@ -84,12 +65,18 @@ end
     assert_equals(true, script.rename_instance_var?(RRB::NS.new('X::B'), '@a', '@f'))
     assert_equals(false,
 		  script.rename_instance_var?(RRB::NS.new('X::B'), '@a', '@b'))
+    assert_equals("X::A already has @b\n", script.error_message)
     assert_equals(false,
 		  script.rename_instance_var?(RRB::NS.new('X::B'), '@a', '@c'))
+    assert_equals("X::B already has @c\n", script.error_message)
     assert_equals(false,
 		  script.rename_instance_var?(RRB::NS.new('X::B'), '@a', '@d'))
+    assert_equals("X::C already has @d\n", script.error_message)
     assert_equals(false,
-		  script.rename_instance_var?(RRB::NS.new('X::B'), '@a', '@e'))
+		  script.rename_instance_var?(RRB::NS.new('X::B'), '@a', 'ee'))
+    assert_equals("ee is not a valid name for instance variables\n",
+                  script.error_message)
+
   end
 
   def test_rename_class_var
@@ -110,10 +97,15 @@ end
   def test_rename_class_var?
     script = RRB::Script.new_from_filenames('samples/rename_class_var_sample.rb')
     assert_equals(false, script.rename_class_var?(RRB::NS['X::B'], '@@a', '@@b'))
+    assert_equals("X::A already has @@b\n", script.error_message)
     assert_equals(false, script.rename_class_var?(RRB::NS['X::B'], '@@a', '@@c'))
+    assert_equals("X::B already has @@c\n", script.error_message)
     assert_equals(false, script.rename_class_var?(RRB::NS['X::B'], '@@a', '@@d'))
+    assert_equals("X::C already has @@d\n", script.error_message)
     assert_equals(false, script.rename_class_var?(RRB::NS['X::B'], '@@a', '@@e'))
-    assert_equals(true, script.rename_class_var?(RRB::NS['X::A'], '@@a', '@@f'))
+    assert_equals("X::D already has @@e\n", script.error_message)
+    assert_equals(false, script.rename_class_var?(RRB::NS['X::A'], '@@a', 'fff'))
+    assert_equals("fff is not a valid name for class variables\n", script.error_message)    
   end
 
   
@@ -121,8 +113,14 @@ end
     script = RRB::Script.new_from_filenames("samples/pullup_method_sample.rb")
     assert_equals(true, script.pullup_method?(RRB::NS['Derived'], 'bar', RRB::NS['Base']))
     assert_equals(false, script.pullup_method?(RRB::NS['Derived'], 'foo', RRB::NS['Base']))
+    assert_equals("Derived#foo uses bar defined at Derived\n", script.error_message)    
     assert_equals(false, script.pullup_method?(RRB::NS['Derived'], 'hoge', RRB::NS['Base']))
+    assert_equals("Derived doesn't have a function called hoge\n", script.error_message)    
     assert_equals(false, script.pullup_method?(RRB::NS['Base'], 'hoge', RRB::NS['Derived']))
+    assert_equals("Derived is not the superclass of Base\n", script.error_message)    
+    assert_equals(false, script.pullup_method?(RRB::NS['Derived'], 'asdf', RRB::NS['Base']))
+    assert_equals("Base already has asdf\n", script.error_message)    
+
   end
 
   def test_pullup_method
@@ -139,9 +137,16 @@ end
     script = RRB::Script.new_from_filenames("samples/pushdown_method_sample.rb")
     assert_equals(true, script.pushdown_method?(RRB::NS['B'], 'x', RRB::NS['C']))
     assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'y', RRB::NS['C']))
+    assert_equals("B#y calls private function \"z\"\n", script.error_message)
     assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'z', RRB::NS['C']))
+    assert_equals("Other function uses B#z\n", script.error_message)
     assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'w', RRB::NS['C']))
+    assert_equals("C already has w\n", script.error_message)
     assert_equals(false, script.pushdown_method?(RRB::NS['C'], 'w', RRB::NS['B']))    
+    assert_equals("B is not the subclass of C\n", script.error_message)
+    assert_equals(false, script.pushdown_method?(RRB::NS['C'], 'asdf', RRB::NS['B']))    
+    assert_equals("C doesn't have a function called asdf\n", script.error_message)
+
     assert_equals(true, script.pushdown_method?(RRB::NS['A'], 'a', RRB::NS['B']))
   end
 
@@ -166,13 +171,28 @@ end
     script = RRB::Script.new_from_filenames("samples/rename_global_var_sample.rb")   
     assert_equals( true, script.rename_global_var?('$x', '$y'))
     assert_equals( false, script.rename_global_var?('$x', '$z'))
+    assert_equals("$z is already used\n", 
+                  script.error_message)
     assert_equals( false, script.rename_global_var?('$x', 'x'))
+    assert_equals("x is not a valid name for global variables\n", 
+                  script.error_message)
     assert_equals( false, script.rename_global_var?('$x', '@x'))
+    assert_equals("@x is not a valid name for global variables\n", 
+                  script.error_message)
     assert_equals( false, script.rename_global_var?('$x', '@@x'))
+    assert_equals("@@x is not a valid name for global variables\n", 
+                  script.error_message)
     assert_equals( false, script.rename_global_var?('$x', 'print'))
+    assert_equals("print is not a valid name for global variables\n", 
+                  script.error_message)
   end
 
   def test_rename_local_var
+    script = RRB::Script.new_from_io( StringIO.new( RENAME_LOCAL_VAR_INPUT ) )
+    script.rename_local_var( RRB::NS.new('Rename'),'method_2','x','yy' )
+    dst = ''      
+    script.result_to_io( dst )
+    assert_equals( RENAME_LOCAL_VAR_OUTPUT, dst )
 
     script = RRB::Script.new_from_filenames("samples/rename_var_sample.rb")   
       script.rename_local_var( RRB::NS.new('Rename'),
@@ -184,7 +204,15 @@ end
   end
 
   def test_rename_local_var?
-    
+    script = RRB::Script.new_from_io( StringIO.new( RENAME_LOCAL_VAR_INPUT ) )
+    assert_equals( true,
+		  script.rename_local_var?(RRB::NS.new('Rename'),'method_2',
+					   'x','yy') )
+
+    assert_equals( false,
+		  script.rename_local_var?(RRB::NS.new('Rename'),'method_1',
+					   'x','y') )
+    assert_equals("y is already used\n", script.error_message)    
     script = RRB::Script.new_from_filenames("samples/rename_var_sample.rb")   
     
     assert_equals( true, script.rename_local_var?( RRB::NS.new('Rename'),
@@ -194,19 +222,29 @@ end
     assert_equals( false, script.rename_local_var?( RRB::NS.new('Rename'),
 							 'method_1',
 							 'z', 'x' ) )
+    assert_equals("x is already used\n", script.error_message)    
     assert_equals( false, script.rename_local_var?( RRB::NS.new('Rename'),
 							 'method_1',
 							  'z', 'i' ) )
+    assert_equals("i is already used\n", script.error_message)    
     # invalid identifier 
     assert_equals( false, script.rename_local_var?( RRB::NS.new('Rename'),
 							 'method_1',
 							  'z', 'Z' ) )
+    assert_equals("Z is not a valid name for local variables\n",
+                  script.error_message)    
     assert_equals( false, script.rename_local_var?( RRB::NS.new('Rename'),
 							 'method_1',
 							  'z', 'print' ) )
+
+    assert_equals("print is already used as a function\n",
+                  script.error_message)
     assert_equals( false, script.rename_local_var?( RRB::NS.new('Rename'),
 							 'method_1',
 							  'z', 'super' ) )
+    assert_equals("super is not a valid name for local variables\n",
+                  script.error_message)
+
     
   end
   def test_rename_method_all
@@ -226,11 +264,55 @@ end
     assert_equals( true, script.rename_method_all?( 'foo', 'foo?' ) )
     # collision with local variable
     assert_equals( false, script.rename_method_all?( 'baz', 'hek' ) )
+    assert_equals("hek is already used as a local variable at B#bar\n",
+                  script.error_message)
+
+    assert_equals( false, script.rename_method_all?( 'baz', 'bar' ) )
+    assert_equals("B already has bar\n", script.error_message)
+    assert_equals( false, script.rename_method_all?( 'print', 'feefoo' ) )
+    assert_equals("Don't rename kernel method\n",
+                  script.error_message)
     # invalid method name 
     assert_equals( false, script.rename_method_all?( 'foo', 'Foo' ) )
+    assert_equals("Foo is not a valid name for methods\n",
+                  script.error_message)
     assert_equals( false, script.rename_method_all?( 'foo', 'foo=' ) )
+    assert_equals("foo= is not a valid name for methods\n",
+                  script.error_message)
     assert_equals( false, script.rename_method_all?( 'foo', 'when' ) )
+    assert_equals("when is not a valid name for methods\n",
+                  script.error_message)
     assert_equals( false, script.rename_method_all?( 'foo', 'foo?fee' ))
+    assert_equals("foo?fee is not a valid name for methods\n",
+                  script.error_message)
+  end
+
+TEST_CLASS_A_METHOD_1 = 
+'  def method_1( a, b )
+    c = 3; @x = 2
+    call( 3, 2 )
+    a.each{|x| print x}
+    yield b+c
+  end
+'
+
+  def test_get_string_of_method
+    script = RRB::Script.new_from_filenames('samples/parser_sample.rb')
+    assert_equals(TEST_CLASS_A_METHOD_1, script.get_string_of_method(RRB::NS.new("TestClassA"), 'method_1'))
+  end
+
+  def test_get_class_on_region
+    script = RRB::Script.new_from_filenames('samples/parser_sample.rb')
+    assert_equals('TestClassA', script.get_class_on_region('samples/parser_sample.rb', 3..6).name)
+    assert_equals(true, script.get_class_on_region('samples/parser_sample.rb', 26..28).nil?)
+    assert_equals('', script.get_class_on_region('samples/parser_sample.rb', 1..1).name)
+  end
+
+  def test_get_method_on_region
+    script = RRB::Script.new_from_filenames('samples/parser_sample.rb')
+    assert_equals('TestClassA#method_1', script.get_method_on_region('samples/parser_sample.rb', 8..11).name)
+    assert_equals(true, script.get_method_on_region('samples/parser_sample.rb', 7..11).nil?)
+    assert_equals("", script.get_method_on_region('samples/parser_sample.rb', 38..38).name)
   end
 
 
