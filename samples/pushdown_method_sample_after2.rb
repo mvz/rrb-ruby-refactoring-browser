cbb5dcd2205ceb03fdb71648e91bbc6d191aa8e0
oxy <>
2004-02-10 12:34:29 +0000

    push down後のインデントのテストを行うためのテストコード

---
+++ samples/pushdown_method_sample_after2.rb
@@ -0,0 +1,28 @@
+samples/pushdown_method_sample.rbclass A
+  def a
+  end
+end
+
+class B < A
+  def y
+    z
+  end
+  def z
+  end
+  def w
+  end
+
+  private :z
+end
+
+class C < B
+  def w
+  end
+  class D < B
+    def x
+      p 1
+      a
+    end
+  end
+end
+-- END --
