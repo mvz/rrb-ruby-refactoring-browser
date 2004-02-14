2ce93ad647cd8bf5f15d6a5a3e1af4dde50a7a0f
ohai <>
2004-02-14 10:52:00 +0000

    TestScript_ExtractSuperclass#test_extract_superclass? TestScript_ExtractSuperclass#test_extract_superclass   Script#extract_superclass,Script#extract_superclass?の引数に新しいクラスを   定義する位置の行番号を与えられるようになったのに対応する TestScript_ExtractSuperclass#test_new_superclass_def   新設   Script#superclass_def のテスト

--- tests/test_extract_superclass.rb
+++ tests/test_extract_superclass.rb
@@ -4,13 +4,14 @@ require 'runit/cui/testrunner'
 require 'rrb/extract_superclass'
 
 class TestScriptFile_ExtractSuperclass < RUNIT::TestCase
-  
+
 end
 
 class TestScript_ExtractSuperclass < RUNIT::TestCase
   INPUT_STR = <<EOS
 /home/heke/temp/file1.rb#{RRB::IO_SPLITTER}
 class A
+
   class D
     class E < A
       def heke
@@ -21,17 +22,20 @@ class A
 end
 
 class B < A
+  
 end
 
 class C < A
 end
 CONST = 3
+
 #{RRB::IO_SPLITTER}#{RRB::IO_TERMINATOR}#{RRB::IO_SPLITTER}
 EOS
 
-  OUTPUT_STR = <<EOS
+  OUTPUT_STR1 = <<EOS
 /home/heke/temp/file1.rb#{RRB::IO_SPLITTER}
 class A
+
   class D
     class E < ::B::NEW
       def heke
@@ -42,6 +46,7 @@ class A
 end
 
 class B < ::B::NEW
+  
 end
 
 class C < ::B::NEW
@@ -51,6 +56,34 @@ class B
   class NEW < ::A
   end
 end
+
+#{RRB::IO_SPLITTER}#{RRB::IO_TERMINATOR}#{RRB::IO_SPLITTER}
+EOS
+
+
+  OUTPUT_STR2 = <<EOS
+/home/heke/temp/file1.rb#{RRB::IO_SPLITTER}
+class A
+
+  class NEW < ::A
+  end
+  class D
+    class E < ::A::NEW
+      def heke
+        p CONST
+      end
+    end
+  end
+end
+
+class B < ::A::NEW
+  
+end
+
+class C < ::A::NEW
+end
+CONST = 3
+
 #{RRB::IO_SPLITTER}#{RRB::IO_TERMINATOR}#{RRB::IO_SPLITTER}
 EOS
 
@@ -60,29 +93,54 @@ EOS
                    script.extract_superclass?( RRB::NS[''], 'NEW',
                                                [RRB::NS['B'],
                                                 RRB::NS['C'],
-                                                RRB::NS['A::D::E']] ))
+                                                RRB::NS['A::D::E']],
+                                               '/home/heke/temp/file1.rb',
+                                               19 ))
     assert_equals( false,
                    script.extract_superclass?( RRB::NS[''], 'NEW',
-                                               [RRB::NS['K']]))
+                                               [RRB::NS['K']],
+                                               '/home/heke/temp/file1.rb',
+                                               19 ))
     assert_equals( false,
                    script.extract_superclass?( RRB::NS[''], 'NEW',
                                                [RRB::NS['B'],
                                                 RRB::NS['C'],
-                                                RRB::NS['A::D']] ))
+                                                RRB::NS['A::D']],
+                                               '/home/heke/temp/file1.rb',
+                                               19 ))
     assert_equals( false,
                    script.extract_superclass?( RRB::NS[''], 'C',
                                                [RRB::NS['B'],
-                                                RRB::NS['A::D']] ))
+                                                RRB::NS['A::D']],
+                                               '/home/heke/temp/file1.rb',
+                                               19 ))
     assert_equals( false,
                    script.extract_superclass?( RRB::NS['A'], 'CONST',
-                                               [RRB::NS['B'],RRB::NS['C']] ))
+                                               [RRB::NS['B'],RRB::NS['C']],
+                                               '/home/heke/temp/file1.rb',
+                                               19 ))
 
     assert_equals( false,
                    script.extract_superclass?( RRB::NS['A'], 'D',
-                                               [RRB::NS['B']] ))
+                                               [RRB::NS['B']],
+                                               '/home/heke/temp/file1.rb',
+                                               19 ))
     assert_equals( false,
                    script.extract_superclass?( RRB::NS['A::D'], 'D',
-                                               [RRB::NS['B']] )) 
+                                               [RRB::NS['B']],
+                                               '/home/heke/temp/file1.rb',
+                                               19 ))
+
+    assert_equals( true,
+                   script.extract_superclass?( RRB::NS['A'], 'NEW',
+                                               [RRB::NS['B']],
+                                               '/home/heke/temp/file1.rb',
+                                               3 ))
+      assert_equals( false,
+                     script.extract_superclass?( RRB::NS['A'], 'NEW',
+                                                 [RRB::NS['B']],
+                                                 '/home/heke/temp/file1.rb',
+                                                 14 ))
   end
 
   def test_extract_superclass
@@ -90,11 +148,35 @@ EOS
 
     script.extract_superclass( RRB::NS['B'], 'NEW',
                                [RRB::NS['A::D::E'],RRB::NS['B'],RRB::NS['C']],
-                               '/home/heke/temp/file1.rb' )
+                               '/home/heke/temp/file1.rb', 19 )
     result = ''; script.result_to_io(result)
-    assert_equals( OUTPUT_STR, result )
+    assert_equals( OUTPUT_STR1, result )
+
+    script = RRB::Script.new_from_io( StringIO.new( INPUT_STR ))
+    script.extract_superclass( RRB::NS['A'], 'NEW',
+                               [RRB::NS['A::D::E'],RRB::NS['B'],RRB::NS['C']],
+                               '/home/heke/temp/file1.rb', 3 )
+    result = ''; script.result_to_io(result)
+    assert_equals( OUTPUT_STR2, result )
   end
+
+  NEW_SUPERCLASS_DEF = [
+    "class D\n",
+    "  class NEW < ::C\n",
+    "  end\n",
+    "end\n",
+  ]
   
+  def test_new_superclass_def
+    script = RRB::Script.new_from_io( StringIO.new( INPUT_STR ))
+    scriptfile = RRB::ScriptFile.new( "", "" )
+    assert_equals( NEW_SUPERCLASS_DEF,
+                   script.superclass_def( RRB::NS["A::D"],
+                                          "NEW",
+                                          RRB::NS["C"],
+                                          RRB::NS["A"] ))
+  end
+
 end
 
 if $0 == __FILE__
