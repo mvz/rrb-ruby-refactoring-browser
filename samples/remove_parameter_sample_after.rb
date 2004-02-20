a387a07789b0237ca3287d0dfff6679062a68a7e
oxy <>
2004-02-20 16:16:38 +0000

    Remove Parameterのサンプル(リファクタリング後)

---
+++ samples/remove_parameter_sample_after.rb
@@ -0,0 +1,21 @@
+class Base
+  def base_function(heke)
+
+  end
+end
+
+class Derived < Base
+  def target_function()
+  end
+
+  def using_parameter_function(heke, doga)
+    p heke
+  end
+
+  def dummy
+    target_function(0)
+    using_parameter_function(0)
+    base_function(0)
+  end
+  
+end
