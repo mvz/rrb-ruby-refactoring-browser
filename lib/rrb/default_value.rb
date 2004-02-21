d72a7722f3c36f3c1d4f2a5d977fe7e61d3596ed
oxy <>
2004-02-21 14:54:04 +0000

    get_class_on_cursor, get_method_on_cursorをcommon_visitor.rbに移動

--- lib/rrb/default_value.rb
+++ lib/rrb/default_value.rb
@@ -6,12 +6,5 @@ module RRB
   
   class Script
 
-    def get_class_on_cursor(path, lineno)
-      get_class_on_region(path, lineno..lineno)
-    end
-
-    def get_method_on_cursor(path, lineno)
-      get_method_on_region(path, lineno..lineno)
-    end
   end
 end
