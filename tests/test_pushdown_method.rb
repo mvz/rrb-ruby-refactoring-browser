3dc0b3e9692302262d15ac387e8796a2cf4ad322
oxy <>
2004-02-21 07:55:06 +0000

    複数のファイルをpushdown methodするときのテストを追加

--- tests/test_pushdown_method.rb
+++ tests/test_pushdown_method.rb
@@ -7,35 +7,127 @@ end
 
 class TestScript_PushdownMethod < RUNIT::TestCase
   def test_pushdown_method?
-    script = RRB::Script.new_from_filenames("samples/pushdown_method_sample.rb")
-    assert_equals(true, script.pushdown_method?(RRB::NS['B'], 'x', RRB::NS['C']))
-    assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'y', RRB::NS['C']))
+    filename = "samples/pushdown_method_sample.rb"
+    lineno = 10
+    script = RRB::Script.new_from_filenames(filename)
+    assert_equals(true, script.pushdown_method?(RRB::NS['B'], 
+'x', RRB::NS['C'], filename, lineno))
+    assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'y', RRB::NS['C'], filename, lineno))
     assert_equals("B#y calls private function \"z\"\n", script.error_message)
-    assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'z', RRB::NS['C']))
+    assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'z', RRB::NS['C'], filename, lineno))
     assert_equals("Other function uses B#z\n", script.error_message)
-    assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'w', RRB::NS['C']))
+    assert_equals(false, script.pushdown_method?(RRB::NS['B'], 'w', RRB::NS['C'], filename, lineno))
     assert_equals("C already has w\n", script.error_message)
-    assert_equals(false, script.pushdown_method?(RRB::NS['C'], 'w', RRB::NS['B']))    
+    assert_equals(false, script.pushdown_method?(RRB::NS['C'], 'w', RRB::NS['B'], filename, lineno))    
     assert_equals("B is not the subclass of C\n", script.error_message)
-    assert_equals(false, script.pushdown_method?(RRB::NS['C'], 'asdf', RRB::NS['B']))    
+    assert_equals(false, script.pushdown_method?(RRB::NS['C'], 'asdf', RRB::NS['B'], filename, lineno))    
     assert_equals("C doesn't have a function called asdf\n", script.error_message)
 
-    assert_equals(true, script.pushdown_method?(RRB::NS['A'], 'a', RRB::NS['B']))
+    assert_equals(true, script.pushdown_method?(RRB::NS['A'], 'a', RRB::NS['B'], filename, lineno))
   end
 
   def test_pushdown_method
-    script = RRB::Script.new_from_filenames("samples/pushdown_method_sample.rb")
-    script.pushdown_method(RRB::NS['B'], 'x', RRB::NS['C'])
+    filename = "samples/pushdown_method_sample.rb"
+    lineno = 10
+    script = RRB::Script.new_from_filenames(filename)
+    script.pushdown_method(RRB::NS['B'], 'x', RRB::NS['C'], filename, lineno)
     dst = ''
     script.result_to_io(dst)
     assert_equals( File.open( 'samples/pushdown_method_sample_after.rb' ).read,
                    dst )    
-    script = RRB::Script.new_from_filenames("samples/pushdown_method_sample.rb")
-    script.pushdown_method(RRB::NS['B'], 'x', RRB::NS['C::D'])
+    script = RRB::Script.new_from_filenames(filename)
+    script.pushdown_method(RRB::NS['B'], 'x', RRB::NS['C::D'], filename, lineno)
     dst = ''
     script.result_to_io(dst)
     assert_equals( File.open( 'samples/pushdown_method_sample_after2.rb' ).read,
                    dst )    
+  end
+
+  INPUT_STR = "\
+/home/yuichi/work/rrb/private/test.rb\C-a
+class Base
+  def hoge
+  end
+end
+
+class Derived < Base
+end
+
+class Derived < Base
+end
+\C-a/home/yuichi/work/rrb/private/test2.rb\C-a
+class Derived < Base
+end
+\C-a-- END --\C-a
+"
+OUTPUT_STR1 = "\
+/home/yuichi/work/rrb/private/test.rb\C-a
+class Base
+end
+
+class Derived < Base
+  def hoge
+  end
+end
+
+class Derived < Base
+end
+\C-a-- END --\C-a
+"
+OUTPUT_STR2 = "\
+/home/yuichi/work/rrb/private/test.rb\C-a
+class Base
+end
+
+class Derived < Base
+end
+
+class Derived < Base
+  def hoge
+  end
+end
+\C-a-- END --\C-a
+"
+OUTPUT_STR3 = "\
+/home/yuichi/work/rrb/private/test.rb\C-a
+class Base
+end
+
+class Derived < Base
+end
+
+class Derived < Base
+end
+\C-a/home/yuichi/work/rrb/private/test2.rb\C-a
+class Derived < Base
+  def hoge
+  end
+end
+\C-a-- END --\C-a
+"
+
+  def test_pushdown_method_plural_files
+    script = RRB::Script.new_from_io( StringIO.new(INPUT_STR ) )
+    script.pushdown_method(RRB::NS['Base'], 'hoge', RRB::NS['Derived'], 
+                           '/home/yuichi/work/rrb/private/test.rb', 7)
+    dst = ''
+    script.result_to_io(dst)
+    assert_equals(OUTPUT_STR1, dst)
+
+    script = RRB::Script.new_from_io( StringIO.new(INPUT_STR ) )
+    script.pushdown_method(RRB::NS['Base'], 'hoge', RRB::NS['Derived'], 
+                           '/home/yuichi/work/rrb/private/test.rb', 10)
+    dst = ''
+    script.result_to_io(dst)
+    assert_equals(OUTPUT_STR2, dst)
+
+    script = RRB::Script.new_from_io( StringIO.new(INPUT_STR ) )
+    script.pushdown_method(RRB::NS['Base'], 'hoge', RRB::NS['Derived'], 
+                           '/home/yuichi/work/rrb/private/test2.rb', 2)
+    dst = ''
+    script.result_to_io(dst)
+    assert_equals(OUTPUT_STR3, dst)
+    
   end
 end
 
