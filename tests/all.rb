a5e00b19451571fce779dbbcf2cc9a3c638ba191
ohai <>
2004-04-08 17:07:52 +0000

    Ruby1.8に付属しているtest/unitのテスト自動実行を抑制するための スクリプトを追加した。 また、実行文全部をif __FILE__ == $0 .... end の中に入れた

--- tests/all.rb
+++ tests/all.rb
@@ -1,16 +1,22 @@
-
-Dir.glob('tests/test*.rb') do |filename|
-  require filename
-end
-
-suite = RUNIT::TestSuite.new
-
-ObjectSpace.each_object(Class) do |klass|
-  if klass.superclass == RUNIT::TestCase then
-    suite.add_test( klass.suite )
-  end
-end
+require 'runit/testcase'
+require 'runit/cui/testrunner'
 
 if __FILE__ == $0 then
+  Dir.glob('tests/test*.rb') do |filename|
+    require filename
+  end
+
+  suite = RUNIT::TestSuite.new
+
+  ObjectSpace.each_object(Class) do |klass|
+    if klass.superclass == RUNIT::TestCase then
+      suite.add_test( klass.suite )
+    end
+  end
+
   RUNIT::CUI::TestRunner.run( suite )
+end
+
+if defined?(Test::Unit) && Test::Unit.respond_to?( :run= )
+  Test::Unit.run=true
 end
