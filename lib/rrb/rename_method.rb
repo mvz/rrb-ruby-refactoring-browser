159b573816c440ead46f523cb11f155faabcfb2e
ohai <>
2004-04-08 08:18:46 +0000

    リファクタリングした。 class RenameMethodVisitor RenameMethodVisitor#initialize RenameMethodVisitor#warning_piece RenameMethodVisitor#rename_method_def RenameMethodVisitor#rename_fcalls RenameMethodVisitor#visit_method   MethodNameのリストをそのまま利用するようにした Script#rename_method?   すべてのメソッドが同じであるかの判定が正しくなかったのでそのバグを修正する

--- lib/rrb/rename_method.rb
+++ lib/rrb/rename_method.rb
@@ -10,45 +10,45 @@ module RRB
   class RenameMethodVisitor < Visitor
 
     def initialize( old_methods, new_method )
-      @classes = old_methods.map{|x| x.namespace}
-      @old_method = old_methods.first.bare_name
+      @old_methods = old_methods
       @new_method = new_method
       @result = []
     end
 
     attr_reader :result
 
-    def warning_piece( namespace, num_spaces )
-      
-      "def #{@old_method}(*arg); \
-raise '#{namespace.name}##{@old_method} is renamed #{@new_method}' end\n" +
+    def warning_piece( namespace, num_spaces, renamed )
+      old_method = renamed.bare_name
+      "def #{old_method}(*arg); \
+raise '#{namespace.name}##{old_method} is renamed #{@new_method}' end\n" +
 	" "*num_spaces
     end
 
-    def rename_method_def( namespace, id, head_keyword )
+    def rename_method_def( namespace, id, head_keyword, renamed )
       @result << Replacer.new_from_id( id, @new_method )
       @result << Replacer.new( id.lineno, head_keyword.head_pointer,
 			      "",
 			      warning_piece( namespace,
-					    head_keyword.head_pointer ) )
+                                             head_keyword.head_pointer,
+                                             renamed ) )
     end
 
-    def rename_fcalls( fcalls )
-      fcalls.find_all(){|fcall| fcall.name == @old_method}.each do |fcall|
+    def rename_fcalls( fcalls, renamed )
+      fcalls.find_all(){|fcall| fcall.name == renamed.bare_name}.each do |fcall|
         @result << Replacer.new_from_id( fcall.body, @new_method )        
       end
     end
     
     def visit_method( namespace, node )
 
-      @classes.each do |classname|
-	if namespace.match?( classname ) &&  @old_method == node.name then
-	  rename_method_def( namespace, node.name_id, node.head_keyword )
-	end
-	if namespace.match?( classname ) then
-	  rename_fcalls( node.fcalls )
-	end
-	
+      @old_methods.each do |renamed|
+        if renamed.match_node?( namespace, node )
+          rename_method_def( namespace, node.name_id, node.head_keyword, renamed )
+        end
+
+        if namespace.match?( renamed.namespace )
+          rename_fcalls( node.fcalls, renamed )
+        end
       end
 
     end
@@ -135,17 +135,18 @@ raise '#{namespace.name}##{@old_method} is renamed #{@new_method}' end\n" +
     end
 
     def rename_method?(  old_methods, new_method )
-      old_method = old_methods.first.bare_name
-      base_classes = old_methods.map{|x| x.namespace}
+      
       unless RRB.valid_method?( new_method )
         @error_message = "#{new_method} is not a valid name for methods\n"
         return false
       end
 
-      unless old_methods.all?{|m| m.bare_name == old_methods}
-        @error_message = "All method should be same"
+      old_method = old_methods.first.bare_name
+      unless old_methods.all?{|m| m.bare_name == old_method}
+        @error_message = "All method should be same name"
+        return false
       end
-      
+
       methods_related_with( old_methods ).each do |method|
         if get_dumped_info[method.namespace].has_method?( new_method )
          @error_message = "#{new_method}: already defined at #{method.namespace.name}\n"
