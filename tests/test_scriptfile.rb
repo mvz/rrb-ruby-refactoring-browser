16cdb173a206b4a7ef7ccc0117cd2baacd622f30
ohai <>
2004-04-22 08:08:57 +0000

    TestScriptFile#move_method TestScriptFile#test_move_method?   以上のテスト用メソッドを削除、

--- tests/test_scriptfile.rb
+++ tests/test_scriptfile.rb
@@ -10,26 +10,6 @@ require 'rrb/move_method'
 
 class TestScriptFile < RUNIT::TestCase
 
-
-  
-
-  def move_method
-    File.open( 'samples/move_method_sample.rb', 'r' ) do |file|
-      script_file = RRB::ScriptFile.new( file, file.path )
-      script_file.move_method('foo', 'X::B', 'X::A')
-      assert_equals( File.open( 'samples/move_method_sample_after.rb' ).read,		    script_file.new_script )
-    end
-  end
-  def test_move_method?
-    File.open( 'samples/move_method_sample.rb', 'r' ) do |file|
-      script_file = RRB::ScriptFile.new( file.read, file.path )
-      assert_equals(true, script_file.move_method?('foo', RRB::NS.new('X::B'), RRB::NS.new('X::A')))
-      assert_equals(false, script_file.move_method?('bar', RRB::NS.new('X::B'), RRB::NS.new('X::A')))
-      assert_equals(false, script_file.move_method?('foos', RRB::NS.new('X::B'), RRB::NS.new('X::A')))
-      assert_equals(false, script_file.move_method?('bar', RRB::NS.new('X::A'), RRB::NS.new('X::B')))
-    end    
-
-  end
 end
 
 if $0 == __FILE__
