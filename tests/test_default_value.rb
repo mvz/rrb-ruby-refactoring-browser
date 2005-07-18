0c01e9a1ab934c2040dfacc4947f83ac7aa8b52f
ohai <>
2005-07-18 14:34:38 +0000

    TestDefaultValue#test_get_class_on_cursor   add test for third parameter 'strict' of get_class_on_cursor

--- tests/test_default_value.rb
+++ tests/test_default_value.rb
@@ -15,8 +15,12 @@ class TestDefaultValue < RUNIT::TestCase
 
   def test_get_class_on_cursor
     script = RRB::Script.new_from_filenames('samples/parser_sample.rb')
-    assert_equals("", script.get_class_on_cursor('samples/parser_sample.rb', 1).name)
-    assert_equals("TestClassA", script.get_class_on_cursor('samples/parser_sample.rb', 3).name)
-    assert_equals("TestClassA::TestClassB", script.get_class_on_cursor('samples/parser_sample.rb', 28).name)
+    assert_equals(RRB::NS[""], script.get_class_on_cursor('samples/parser_sample.rb', 1))
+    assert_equals(RRB::NS["TestClassA"], script.get_class_on_cursor('samples/parser_sample.rb', 3))
+    assert_equals(RRB::NS["TestClassA::TestClassB"], script.get_class_on_cursor('samples/parser_sample.rb', 28))
+
+    assert_nil(script.get_class_on_cursor('samples/parser_sample.rb',2))
+    assert_nil(script.get_class_on_cursor('samples/parser_sample.rb',27))
+    assert_equals(RRB::NS["TestClassA"], script.get_class_on_cursor('samples/parser_sample.rb',27, false))
   end
 end
